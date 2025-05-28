3a1c8b855cacfcedf4866bd2c2163d38f57036b9
Vangelis-26 <mourier.matthieu@gmail.com>
2025-05-28 16:19:10 +0200

    Adding authentication verification for the book management routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
+
 const bookCtrl = require("../controllers/book");
 
-router.post("/", bookCtrl.createBook);
-router.get("/", bookCtrl.allBooks);
-router.get("/:id", bookCtrl.getOneBook);
-router.put("/:id", bookCtrl.updateBook);
-router.delete("/:id", bookCtrl.deleteBook);
+router.post("/", auth, bookCtrl.createBook);
+router.get("/", auth, bookCtrl.allBooks);
+router.get("/:id", auth, bookCtrl.getOneBook);
+router.put("/:id", auth, bookCtrl.updateBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
