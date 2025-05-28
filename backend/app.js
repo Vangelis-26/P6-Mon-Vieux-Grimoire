4fc622fd287eef95a80037aa5a5ab3f57ef36745
Vangelis-26 <mourier.matthieu@gmail.com>
2025-05-28 14:42:48 +0200

    Adding the Book model and API routes for book management

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const Book = require("./models/book");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -10,49 +29,89 @@ mongoose
   .then(() => console.log("Connecté à MongoDB"))
   .catch((err) => console.error("Erreur de connexion à MongoDB", err));
 
-const app = express();
-
-app.use(express.json());
-
+// ----- POST ----- //
 app.post("/api/books", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Objet créé !",
+  console.log("Corps de la requête reçu :", req.body);
+  delete req.body._id;
+  const book = new Book({
+    ...req.body,
   });
+  book
+    .save()
+    .then((createdBook) => {
+      res.status(201).json({
+        message: "Livre ajouté avec succès",
+        bookId: createdBook._id,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Erreur lors de l'ajout du livre",
+        error: error.message,
+      });
+    });
 });
 
+// ----- GET ----- //
 app.get("/api/books", (req, res, next) => {
-  const books = [
-    {
-      id: "1",
-      userId: "clc4wj5lh3gyi0ak4eq4n8syr",
-      title: "Milwaukee Mission",
-      author: "Elder Cooper",
-      imageUrl: "https://via.placeholder.com/206x260",
-      year: 2021,
-      genre: "Policier",
-      ratings: [
-        {
-          userId: "1",
-          grade: 5,
-        },
-        {
-          userId: "1",
-          grade: 5,
-        },
-        {
-          userId: "clc4wj5lh3gyi0ak4eq4n8syr",
-          grade: 5,
-        },
-        {
-          userId: "1",
-          grade: 5,
-        },
-      ],
-      averageRating: 3,
-    },
-  ];
-  res.status(200).json(books);
+  Book.find()
+    .then((books) => {
+      res.status(200).json(books);
+      console.log(books);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Erreur lors de la récupération des livres",
+        error: error.message,
+      });
+    });
+});
+
+// ----- GET one book by ID ----- //
+app.get("/api/books/:id", (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Livre non trouvé" });
+      }
+    })
+    .catch((error) => {
+      c;
+      res.status(500).json({
+        message: "Erreur lors de la récupération du livre",
+        error: error.message,
+      });
+    });
+});
+
+// ----- PUT ----- //
+app.put("/api/books/:id", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => {
+      res.status(200).json({ message: "Livre mis à jour avec succès" });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Erreur lors de la mise à jour du livre",
+        error: error.message,
+      });
+    });
+});
+
+// ----- DELETE ----- //
+app.delete("/api/books/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({ message: "Livre supprimé avec succès" });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Erreur lors de la suppression du livre",
+        error: error.message,
+      });
+    });
 });
 
 module.exports = app;
